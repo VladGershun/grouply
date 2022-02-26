ade89155705a75303900e97ca0177064f106c1fc
VladGershun <VladYGershun@Yahoo.com>
2022-02-26 15:59:00 -0800

    Styling for homepage.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import SignUpForm from './pages/SignUpForm';
 
 const App = () => {
   return(
     <div>
-      <SignUpForm />
+      <HomePage />
+      {/* <SignUpForm /> */}
     </div>
   );
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,27 @@ import '../styles/home-page.css'
 const HomePage = () => {
     return(
         <>
-
+            <div className = 'home-background'>
+                <div className = 'user-info'>
+                    
+                </div>
+                <div className = 'social-info'>
+    
+                </div>
+    
+                <div className = 'menu-bar'>
+    
+                </div>
+    
+                <div className = 'calander-view'>
+    
+                </div>
+    
+                <div className = 'chat-box'>
+    
+                </div> 
+            </div>
+            
         </>
     )
 }
