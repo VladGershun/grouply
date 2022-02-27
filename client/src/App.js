b951efc3152184a3376d9dd0c73245489b09e7dc
VladGershun <VladYGershun@Yahoo.com>
2022-02-26 22:33:35 -0800

    added profile and chat box.

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,15 @@ import SignUpForm from './pages/SignUpForm';
 const App = () => {
   return(
     <div>
-      <HomePage />
+      <HomePage 
+        profileImage = 'https://cdn.pixabay.com/user/2013/11/05/02-10-23-764_250x250.jpg'
+        firstName = 'Vlad'
+        lastName = 'Gershun'
+        username = 'vladgershun'
+        about = 'Probably eating food...'
+        currentTime  = '5:07 PM'
+        date = 'Feb 25, 2022'
+      />
       {/* <SignUpForm /> */}
     </div>
   );
