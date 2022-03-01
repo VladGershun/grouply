76fedae002502af10e8107f62963946902e66bd4
kyle.cuyos13 <kyle.cuyos13@gmail.com>
2022-03-01 01:18:27 -0800

    progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
 import HomePage from './pages/HomePage';
 import SignUpForm from './pages/SignUpForm';
+import LogInForm from './pages/LogInForm';
 
 const App = () => {
   return(
     <div>
-      <HomePage 
+      {/* <HomePage 
         profileImage = 'https://res.cloudinary.com/demo/image/twitter_name/BillClinton.jpg'
         firstName = 'Vlad'
         lastName = 'Gershun'
@@ -13,10 +15,13 @@ const App = () => {
         about = 'Probably eating food...'
         currentTime  = '5:07 PM'
         date = 'Feb 25, 2022'
-      />
+      /> */}
       {/* <SignUpForm 
         errorMsg = 'Test error'
       /> */}
+      <LogInForm.js>
+
+      </LogInForm.js>
     </div>
   );
 }
